8ca9b7e4e57bb603af33dea793fe79bef900e4d8
Hawkins <hawkinszhao@outlook.com>
2016-07-08 18:37:18 +0800

    Add login window

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
 "use strict"
 const electron = require('electron')
 const path = require('path')
-const {app, BrowserWindow, webFrame} = require('electron')
+const {app, BrowserWindow, webFrame, ipcMain} = require('electron')
 
-let noteWindow
+let noteWindow, loginWindow
 
 function createWindow() {
     noteWindow = new BrowserWindow({
-        title: "ElectronicNote",
+        title: "锤子便签",
         width: 1100,
-        height: 700,
+        height: 600,
         frame: true,
+        center: true,
         autoHideMenuBar: true,
+        useContentSize: true,
+        titleBarStyle: 'hidden-inset',
+        show: false,
         webPreferences: {
             javascript: true,
             webSecurity: false,
             nodeIntegration: false,
-            preload: path.join(__dirname, './lib/js/app.js')
+            preload: path.join(__dirname, './lib/Window/BrowserWindow/js/app.js')
         }
     })
 
+    loginWindow = new BrowserWindow({
+        title: "登录",
+        width: 350,
+        height: 500,
+        frame: true,
+        center: true,
+        maximizable: false,
+        alwaysOnTop: true,
+        resizable: false,
+        autoHideMenuBar: true,
+        titleBarStyle: 'hidden-inset',
+        webPreferences: {
+            javascript: true,
+            webSecurity: false,
+            nodeIntegration: false,
+            preload: path.join(__dirname, './lib/Window/loginWindow/js/login.js')
+        }
+    })
+
+    loginWindow.loadURL(`file://${__dirname}/lib/Window/loginWindow/login.html`)
     noteWindow.loadURL(`http://note.t.tt`)
     // noteWindow.webContents.openDevTools()
+    // loginWindow.webContents.openDevTools()
 
     noteWindow.on('closed', () => {
         noteWindow = null
     })
+    loginWindow.on('closed', () => {
+        loginWindow = null
+    })
+
+
 }
 
 app.on('ready', createWindow)
@@ -42,4 +72,14 @@ app.on('activate', function () {
   if (noteWindow === null) {
     createWindow()
   }
+})
+
+ipcMain.on('notes-did-login', (event, arg) => {
+    if (arg) {
+        loginWindow.webContents.send('page-will-load', 'true')
+        setTimeout(() => {
+            loginWindow.hide()
+            noteWindow.show()
+        }, 2000)
+    }
 })
