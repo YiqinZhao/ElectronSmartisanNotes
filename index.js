2aa24c1dc6f3db7426bcd62fb7a26d4babffbb69
Hawkins <hawkinszhao@outlook.com>
2016-07-09 12:26:49 +0800

    Fix title bar bugs and network connection bugs

--- index.js
+++ index.js
@@ -3,27 +3,10 @@ const electron = require('electron')
 const path = require('path')
 const {app, BrowserWindow, webFrame, ipcMain} = require('electron')
 
-let noteWindow, loginWindow
+let noteWindow = null, loginWindow = null
+var internetFlag = false
 
-function createWindow() {
-    noteWindow = new BrowserWindow({
-        title: "锤子便签",
-        width: 1100,
-        height: 600,
-        frame: true,
-        center: true,
-        autoHideMenuBar: true,
-        useContentSize: true,
-        titleBarStyle: 'hidden-inset',
-        show: false,
-        webPreferences: {
-            javascript: true,
-            webSecurity: false,
-            nodeIntegration: false,
-            preload: path.join(__dirname, './lib/Window/BrowserWindow/js/app.js')
-        }
-    })
-
+function createWindow(isConnected) {
     loginWindow = new BrowserWindow({
         title: "登录",
         width: 350,
@@ -44,34 +27,78 @@ function createWindow() {
     })
 
     loginWindow.loadURL(`file://${__dirname}/lib/Window/loginWindow/login.html`)
-    noteWindow.loadURL(`http://note.t.tt`)
-    // noteWindow.webContents.openDevTools()
     // loginWindow.webContents.openDevTools()
 
-    noteWindow.on('closed', () => {
-        noteWindow = null
-    })
     loginWindow.on('closed', () => {
         loginWindow = null
     })
 
-
+    if (isConnected) {
+        noteWindow = new BrowserWindow({
+            title: "锤子便签",
+            width: 1100,
+            minWidth: 1025,
+            height: 600,
+            frame: true,
+            center: true,
+            autoHideMenuBar: true,
+            useContentSize: true,
+            titleBarStyle: 'hidden-inset',
+            show: false,
+            webPreferences: {
+                javascript: true,
+                webSecurity: false,
+                nodeIntegration: false,
+                preload: path.join(__dirname, './lib/Window/BrowserWindow/js/app.js')
+            }
+        })
+        noteWindow.loadURL(`http://note.t.tt`)
+        // noteWindow.webContents.openDevTools()
+        noteWindow.on('closed', () => {
+            noteWindow = null
+        })
+        internetFlag = true
+    }
 }
 
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
+app.on('ready', () => {
+    require('dns').resolve('note.t.tt', function(err) {
+        if (err) {
+            if (noteWindow === null) {
+              createWindow(false)
+            }
+        }
+        else {
+            if (noteWindow === null) {
+              createWindow(true)
+            }
+        }
+    })
 })
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (noteWindow === null) {
-    createWindow()
-  }
+
+  require('dns').resolve('note.t.tt', function(err) {
+      if (err) {
+          if (noteWindow === null) {
+            createWindow(false)
+          }
+      }
+      else {
+          if (noteWindow === null) {
+            createWindow(true)
+          }
+      }
+  })
+
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
 })
 
 ipcMain.on('notes-did-login', (event, arg) => {
@@ -88,5 +115,11 @@ ipcMain.on('notes-did-login', (event, arg) => {
             loginWindow.hide()
             noteWindow.show()
         }, 1500)
+    }
+})
+
+ipcMain.on('login-did-show', (event, arg) => {
+    if (!internetFlag) {
+        loginWindow.webContents.send('internet-connected', 'false')
     }
 })
